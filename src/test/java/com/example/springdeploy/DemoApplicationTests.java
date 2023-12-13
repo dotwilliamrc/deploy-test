75d281dddea0ce4362455a9717d238f4af26c8d8
William Christopher Rendon Castruita <williamcastruita@outlook.com>
2023-12-12 19:32:21 -0600

    test

--- src/test/java/com/example/springdeploy/DemoApplicationTests.java
+++ src/test/java/com/example/springdeploy/DemoApplicationTests.java
@@ -6,11 +6,11 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class DemoApplicationTests {
 
-	@Test
-	void contextLoads() {
+    //	@Test
+	//void contextLoads() {
         //        System.getenv().forEach((key, value) -> {
         //      System.out.println(key + "=" + value);
         //  });
-	}
+	//}
 
 }
